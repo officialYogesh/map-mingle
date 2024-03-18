7b1db2a4040036005f3cef8d238fc96dd91dd074
Yogesh Patil <officialyogeshp@gmail.com>
2024-03-18 16:23:46 -0400

    Added: Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import NavigationIcon from "@mui/icons-material/Navigation";
+
+function Header() {
+  return (
+    <AppBar position="static" color="default">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <NavigationIcon sx={{ display: { xs: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            MAP MINGLE
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Header;
