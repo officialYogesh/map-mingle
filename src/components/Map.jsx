bae9a4d57a96354073028ab880543e66d5644c4a
Yogesh Patil <officialyogeshp@gmail.com>
2024-03-18 20:27:08 -0400

    Added: Markers with Custom Icon and Popups

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Chip from "@mui/material/Chip";
+import { Icon } from "leaflet";
 
 const Map = () => {
+  const [markers, setMarkers] = useState([
+    {
+      geocode: [33.97791908665812, -117.33147563686414],
+      popUp: "Getaway Cafe",
+    },
+    {
+      geocode: [33.974738370122274, -117.32818699929638],
+      popUp: "The Habit Burger Grill",
+    },
+    {
+      geocode: [33.975251919150686, -117.33806951443785],
+      popUp: "Mongolian BBQ",
+    },
+    {
+      geocode: [33.97287587241276, -117.33054502552945],
+      popUp: "The Barn",
+    },
+    {
+      geocode: [33.97761408780611, -117.3382975261192],
+      popUp: "Cravin' Crab Haus",
+    },
+  ]);
+
+  const customMarkerIcon = new Icon({
+    iconUrl: require("../img/marker-icon.png"),
+    iconSize: [38, 38],
+  });
+
   return (
-    <MapContainer center={[33.95335, -117.396156]} zoom={13}>
+    <MapContainer center={[33.97386562552932, -117.32761379253915]} zoom={13}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
+      {markers.map((marker) => (
+        <Marker
+          key={marker.geocode}
+          position={marker.geocode}
+          icon={customMarkerIcon}
+        >
+          <Popup>{marker.popUp}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
